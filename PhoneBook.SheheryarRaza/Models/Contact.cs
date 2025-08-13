6fb7b8ac6a8186c5f843e99c8db51743ddd659f9
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-08-13 21:36:15 +0500

    Added Model

---
+++ PhoneBook.SheheryarRaza/Models/Contact.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.SheheryarRaza.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [Column("PhoneNumber")] // Explicitly define column name if needed
+        public string PhoneNumber { get; set; }
+    }
+}
