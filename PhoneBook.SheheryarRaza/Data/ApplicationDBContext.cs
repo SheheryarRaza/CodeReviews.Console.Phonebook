ae51f59e98ad8f9c99102ac610d264695a6b74e0
SheheryarRaza <117226030+SheheryarRaza@users.noreply.github.com>
2025-08-13 21:36:36 +0500

    Added DBContexr

---
+++ PhoneBook.SheheryarRaza/Data/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PhoneBook.SheheryarRaza.Models;
+
+namespace PhoneBook.SheheryarRaza.Data
+{
+    public class ApplicationDBContext: DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-JMBBD5O\\SQLEXPRESS; Database = PhoneBook; User ID = Blocks-Administrator; Password = sheri1234; TrustServerCertificate = True; ");
+        }
+    }
+}
